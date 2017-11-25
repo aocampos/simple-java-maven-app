8de59729871052d9aadbcfe8f8b451995c14d062
Alex Campos <alexcampos@ciandt.com>
2017-11-25 13:16:30 -0200

    changes

---
+++ src/main/docker/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "********************************************************"
+echo "Starting My app"
+echo "********************************************************"
+java -jar /usr/local/my-app/@project.build.finalName@.jar
